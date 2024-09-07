b097f3626847f29eb5f090980ae33dd9436e662e
Kshitij Jhunjhunwala <kshjhun@gmail.com>
2024-09-07 17:29:57 -0500

    removing old files and merging git histories

--- talk-to-data/app/head.js
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-    return (
-      <>
-        <title>File Upload App</title>
-        <meta name="description" content="Upload and process files" />
-        <link rel="icon" href="/favicon.ico" />
-      </>
-    );
-  }
